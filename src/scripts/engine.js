71aca0405357b88f6e8012699c9c3870577c68cf
Rubens Carius <rubenscarius@msn.com>
2024-01-08 22:07:01 -0300

    game base html and css

---
+++ src/scripts/engine.js
@@ -0,0 +1,26 @@
+ const state = {
+    score: {
+        playerScore: 0,
+        computerScore: 0,
+        socoreBox: document.getElementById("score_points"),
+    },
+    cardSprites:{
+        avatar: document.getElementById("card-image"),
+        name: document.getElementById("card-name"),
+        type: document.getElementById("card-type"),
+    },
+    fieldCards:{
+        player: document.getElementById("player-field-card"),
+        computer: document.getElementById("computer-field-card"),
+    },
+    actions:{
+        button: document.getElementById("next-duel")
+    },
+ }
+ 
+ 
+
+ function init() {
+
+ }
+ init();
